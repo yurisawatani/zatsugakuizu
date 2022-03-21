8f8a04e73ca937a0775403a647bd46bbd1fd2de8
沢谷優理 <yuri@sawatani.jp>
2022-03-21 10:08:14 +0900

    チョコ

---
+++ tyoco/tyoco.go
@@ -0,0 +1,21 @@
+package tyoco
+
+type Tyoco struct {
+	Question string
+	Answer   string
+}
+
+var Xlist = []Tyoco{
+	{
+		Question: "4+5=",
+		Answer:   "9",
+	},
+	{
+		Question: "私の誕生月は？",
+		Answer:   "9",
+	},
+	{
+		Question: "私は2022年4月に中学何年生になる？",
+		Answer:   "3",
+	},
+}
